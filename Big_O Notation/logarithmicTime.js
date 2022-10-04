27b1a3631706051c38cb0691fb92025eb8342fc2
Trend20 <enockomondi305@gmail.com>
2022-10-04 15:56:14 +0300

    logarithmic time

---
+++ Big_O Notation/logarithmicTime.js
@@ -0,0 +1,16 @@
+/*
+Logarithmic algorithm
+
+1. An algorithm is said to run in logarithmic time if its time execution 
+   is proportional to the logarithm of the input size, and we mark it as O(log n).
+
+2. O(log n) algorithms never look at all the elements in the input. logarithmic 
+   time complexities usually apply to algorithms that eliminate large amounts of input elements in each step.
+
+ */
+
+// Example: BinarySearch Algorithm
+
+
+
+// O(log n) => Time Complexity
