52cb7fb1b01dbb4428a8f867263f12e3ab529b96
Trend20 <enockomondi305@gmail.com>
2022-10-19 19:03:13 +0300

    bug fix

---
+++ ProblemSolvingPatterns/FrequencyCouterPattern.js
@@ -0,0 +1,6 @@
+// EXAMPLE
+/*
+  Write a function called same which accepts two arrays and the function should return true if every value in
+  the array has its corresponding values squared in the second array. 
+  THE FREQUENCY OF VALUES MUST BE THE SAME
+*/
--- Searching/BinarySearch.js
+++ Searching/BinarySearch.js
@@ -32,4 +32,4 @@ function binarySearch(arr, target){
   return -1;
 }
 
-console.log(linearSearch([1, 3, 5, 7, 9], 6));
+console.log(binarySearch([1, 3, 5, 7, 9], 6));
