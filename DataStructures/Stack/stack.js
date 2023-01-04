ffc4cccb25cdafc915bd0f15bd180bdc9e90e213
Trend20 <enockomondi305@gmail.com>
2023-01-04 22:07:58 +0300

    stack

---
+++ DataStructures/Stack/stack.js
@@ -0,0 +1,18 @@
+/*
+Stack is a linear data structure. It is an abstract data type that holds data in a sequence.
+It works with the LIFO(Last In First Out) principle where the last item that is added to the stack is accessed first
+and the first item that has been added to the stack is accessed last.
+
+Application of the Stack
+A Stack can be used for evaluating expressions consisting of operands and operators.
+Stacks can be used for Backtracking, i.e., to check parenthesis matching in an expression.
+It can also be used to convert one form of expression to another form.
+It can be used for systematic Memory Management.
+
+There are 3 main operations that can be done on a stack:
+
+Push: Adding an item to the peek stack.
+Pop: Removing an item from the peek of the stack.
+Peek: Returns the top item.
+
+ */
